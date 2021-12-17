10482a1e916d5d6042b593f9a20f2265ffb9a77e
William Ruan <89553051+naurw@users.noreply.github.com>
2021-12-16 23:11:36 -0500

    Updated test
    Updated test

--- test_streamlit.py
+++ test_streamlit.py
@@ -13,22 +13,20 @@ import pandas as pd
 import numpy as np
 import plotly.express as px
 
-
 @st.cache
 def load_hospitals():
-    hospital = pd.read_csv('/Users/William/Desktop/AHI_DataSci_507/Deployment_Streamlit/hospital_info.csv')
+    hospital = pd.read_csv('https://raw.githubusercontent.com/hantswilliams/AHI_STATS_507/main/Week13_Summary/output/df_hospital_2.csv')
     return hospital
 
 @st.cache
-def load_inpatient():
-    inpatient = pd.read_csv('/Users/William/Desktop/AHI_DataSci_507/Deployment_Streamlit/inpatient_2015.csv')
-    return inpatient
-
-@st.cache
 def load_outpatient():
-    outpatient = pd.read_csv('/Users/William/Desktop/AHI_DataSci_507/Deployment_Streamlit/outpatient_2015.csv')
+    outpatient = pd.read_csv('https://raw.githubusercontent.com/hantswilliams/AHI_STATS_507/main/Week13_Summary/output/df_outpatient_2.csv')
     return outpatient
 
+@st.cache
+def load_inpatient():
+    inpatient = pd.read_csv('https://raw.githubusercontent.com/hantswilliams/AHI_STATS_507/main/Week13_Summary/output/df_inpatient_2.csv')
+    return inpatient
 
 #Load the data:     
 hospital = load_hospitals()
@@ -36,10 +34,10 @@ inpatient = load_inpatient()
 outpatient = load_outpatient()
 
 st.header('Hospital Data Preview')
-st.dataframe(hospital_info)
+st.dataframe(hospital)
 
 st.header('Outpatient Data Preview')
-st.dataframe(outpatient2015)
+st.dataframe(outpatient)
 
 st.header('Inpatient Data Preview')
-st.dataframe(inpatient2015)
+st.dataframe(inpatient)
