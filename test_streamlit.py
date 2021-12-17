337a586b19ab7c102d336db00724f31e661d8e0d
William Ruan <89553051+naurw@users.noreply.github.com>
2021-12-16 23:13:43 -0500

    Updated test
    New dependencies

--- test_streamlit.py
+++ test_streamlit.py
@@ -41,3 +41,24 @@ st.dataframe(outpatient)
 
 st.header('Inpatient Data Preview')
 st.dataframe(inpatient)
+
+
+st.title('A Deep Dive on Hospital Performance and Payments in NY Inpatient and Outpatient Facilities')
+st.markdown('By: William Ruan')
+st.markdown('Last updated: :smile:')
+st.markdown('The purpose of this exploratory analysis is to compare hospital performance across New York utilizing Python for programming and Streamlit for deployment. The variables used in this case study report includes mortality rate, safety of care, and patient experience. In addition, average total payments for providers in inpatient and outpatient facilities will be compared. This report will also provide a dive into the hospital performance of and total payments to: Stony Brook University Hospital, Maimonides Medical Center, and Mount Sinai Hospital.')
+
+#Creating Menu to look at the datasets used in this dashboard
+st.markdown('Three national-level datasets were used in this report. To review each dataset, please click on the select bar below. However, please note that due to the number of observations within these dataset, loading time may take some time. Please give it a moment and take a sip of your favorite drink. :smile:')
+selectbar = st.selectbox('Select Dataset', ("Hospital Experience", "Inpatient Payments", "Outpatient Payments"))
+
+
+def get_dataset(selectbar):
+    if selectbar == 'Hospital Experience':
+        st.write(hospital)
+    if selectbar == 'Inpatient Payments':
+        st.write(inpatient)
+    if selectbar == 'Outpatient Payments':
+        st.write(outpatient)
+
+st.write(get_dataset(selectbar))
